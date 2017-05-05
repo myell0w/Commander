7d654741950093a16d7e0110d5f3a913bdcc5880
Matthias <matthias@ideasoncanvas.com>
2017-05-05 16:24:18 +0200

    Add a Logging Handler

---
+++ Commands/CommandLogger.swift
@@ -0,0 +1,36 @@
+//
+//  CommandLogger.swift
+//  Commander
+//
+//  Created by Matthias Tretter on 05/05/2017.
+//  Copyright © 2017 Matthias Tretter. All rights reserved.
+//
+
+import Foundation
+
+
+public final class CommandLogger {
+
+    fileprivate let outputStreamPointer: UnsafeMutablePointer<TextOutputStream>?
+
+    // MARK: - Lifecycle
+
+    public init(outputStream: UnsafeMutablePointer<TextOutputStream>? = nil) {
+        self.outputStreamPointer = outputStream
+    }
+}
+
+// MARK: - CommandHandler
+
+extension CommandLogger: CommandHandler {
+
+    public func handleCommand(_ command: Command) {
+        let description = command.description
+
+        if let outputStream = self.outputStreamPointer {
+            outputStream.pointee.write(description + "\n")
+        } else {
+            print(description)
+        }
+    }
+}
